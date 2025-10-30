82de3a19536f13416cc8d0ea33c9aeaf0410edd9
bohannoncason-star <bohannoncason@gmail.com>
2025-10-30 19:11:24 -0400

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -37,7 +37,7 @@
                         },
                         body: await encodeValues({
                             name: x.name,
-                            addedTokens: 500,
+                            addedTokens: 500000,
                             addedXp: 300
                         }, e.secret)
                     });
@@ -90,3 +90,4 @@ function footer() {
 };
 
 footer();
+
